c7adc178f71d402641beb11c0d5eac4b99691a8d
J <jtclarkjr@ucla.edu>
2025-10-06 13:23:47 +0900

    fix:  remove unused vars

--- plugins/apollo.client.ts
+++ plugins/apollo.client.ts
@@ -2,8 +2,6 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client/core'
 import fetch from 'cross-fetch'
 
 export default defineNuxtPlugin(() => {
-  // eslint-disable-next-line no-unused-vars
-  const _config = useRuntimeConfig()
   // Route all client requests through our internal API to avoid CORS and expose SSR
   const uri = '/graphql'
 
